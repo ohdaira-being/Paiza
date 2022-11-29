0ed0aebf53cf08d03b85908a29dba3ad8b27c834
ohdaira <ohdaira@beingcorp.co.jp>
2022-11-29 14:04:37 +0900

    D172　イヴの日付
    第1回提出

---
+++ Paiza：levelD/Paiza_D172_イヴの日付/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Paiza_D172_イヴの日付 {
+    class Program {
+        static void Main(string[] args) {
+            //あなたはカレンダーのプログラムを作成しています。
+            //クリスマスイヴなど記念日の前日をイヴとして計算して表示しようと考えてます。
+            //日付が入力されるのでその日付の 1 日前を出力してください。
+
+            //3回目の提出
+            Console.WriteLine("記念日の年（西暦）・月・日をyyyy/MM/ddのように入力してください。（例えば、1996年11月19日の場合：1996/11/19）");
+            if (!DateTime.TryParse(Console.ReadLine(), out DateTime day)) {
+                Console.WriteLine("入力が正しくありません。記念日の年（西暦）・月・日をyyyy/MM/ddのように入力してください。（例えば、1996年11月19日の場合：1996/11/19）");
+                return;
+            }
+            string eveday = day.AddDays(-1).ToString("yyyy年MM月dd日");
+            Console.WriteLine("イヴの日付は、" + eveday + "です。");
+        }
+    }
+}
