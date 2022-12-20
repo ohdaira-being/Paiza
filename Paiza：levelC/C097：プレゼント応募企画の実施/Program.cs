a1055cc5843e7f9942673d59241d4727a4598bcb
ohdaira <ohdaira@beingcorp.co.jp>
2022-12-20 10:29:20 +0900

    C097：プレゼント応募企画の実施
    一回目の提出

---
+++ Paiza：levelC/C097：プレゼント応募企画の実施/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace C097_プレゼント応募企画の実施 {
+    class Program {
+        static void Main(string[] args) {
+            int[] input =  Console.ReadLine().Split(' ').Select(value => int.Parse(value)).ToArray();
+            for(int n =1; n <= input[0]; n++){
+                if(n % input[1] == 0 && n % input[2] == 0){
+                    Console.WriteLine("AB");
+                }else if(n % input[1] == 0){
+                    Console.WriteLine("A");
+                }else if (n % input[2] == 0) {
+                    Console.WriteLine("B");
+                }else{
+                    Console.WriteLine("N");
+                }
+            }
+        }
+    }
+}
